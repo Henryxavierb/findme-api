e3246ee46e5fb54a58cd121f6da7fe66078c750c
Henrique Barros <henriquexavierb@gmail.com>
2020-06-09 20:48:46 -0300

    [ Feat ] Body email request

---
+++ src/utils/template.js
@@ -0,0 +1,50 @@
+module.exports = {
+  template(expiredToken, userName) {
+    return `
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+      <head>
+        <meta charset="UTF-8">
+        <title> Forgot Email </title>
+      </head>
+      <body>
+        <div 
+          style="max-width: 700px; 
+            max-height: 800px; 
+            color: #454545;
+            margin: 0 auto"
+          >
+          <div style="margin: 20px 50px">
+            <h1> Redefinição de senha </h1>
+          </div>
+    
+          <h1 style="margin: 30px 50px 20px"> Olá, ${userName} </h1>
+          <p style="margin: 10px 50px 20px">
+            Lamentamos que esteja tendo problemas ao tentar iniciar a sessão.
+          </p>
+          <p style="margin: 10px 50px 20px">
+            Redefina sua senha para que você possa continuar desfrutando da plataforma.
+          </p>
+          <p style="margin: 2px 50px 20px">
+            Vale lembrar que esse link expira em 1h depois de enviado.
+          </p>
+    
+          <p style="margin: 10px 50px 20px">
+            Caso você não tenha desejado redefinir sua senha, apenas ignore este email.
+          </p>       
+    
+          <div style="margin: 10px 50px 20px">
+            <p style="font-weight: bold">Copie o seguinte token para redefinir sua senha:</p>
+            <p>${expiredToken}</p>
+          </div>
+          
+          <a href="https://youtube.com" style="margin: 0 50px">
+            Redefinir senha
+          </a>
+  
+        </div>
+      </body>
+    </html>
+    `;
+  },
+};
