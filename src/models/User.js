b89c0b7d18976435621daeaf6b11a902ba1a11ca
Henrique Barros <henriquexavierb@gmail.com>
2020-06-09 20:46:53 -0300

    [ Chore ] Create user model

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require("sequelize");
+
+class User extends Model {
+  static init(connection) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        photo: DataTypes.STRING,
+        profile: DataTypes.STRING,
+        password: DataTypes.STRING,
+        expiredToken: DataTypes.STRING,
+      },
+      { sequelize: connection }
+    );
+  }
+  static associate(models) {
+    this.hasMany(models.Event, {
+      foreignKey: "user_id",
+      as: "events",
+    });
+  }
+}
+
+module.exports = User;
