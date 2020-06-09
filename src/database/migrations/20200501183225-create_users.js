0b49a2fcb982ea6ede155501870d84680a619ad4
Henrique Barros <henriquexavierb@gmail.com>
2020-06-09 20:46:44 -0300

    [ Chore ] Create user migration

---
+++ src/database/migrations/20200501183225-create_users.js
@@ -0,0 +1,53 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      profile: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      photo: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      expired_token: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      time_expired: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("users");
+  },
+};
