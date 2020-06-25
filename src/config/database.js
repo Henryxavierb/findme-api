e0a1fa078569ca3acbe50ead1f7ee0bd785f5b99
Henrique Barros <henriquexavierb@gmail.com>
2020-06-25 14:37:01 -0300

    Remove break lines

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-  host: process.env.DATABASE_HOST,
   dialect: "postgres",
   database: process.env.DATABASE,
+  host: process.env.DATABASE_HOST,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
-  define: {
-    timestamp: true,
-    underscored: true,
-  },
+  define: { timestamp: true, underscored: true },
 };
