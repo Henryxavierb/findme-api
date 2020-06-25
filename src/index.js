9bdab3eea8c660afc05dfa65fbb4c01ecff358ed
Henrique Barros <henriquexavierb@gmail.com>
2020-06-25 16:54:00 -0300

    Config express to send static files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
+const path = require("path");
 require("./database");
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+app.use("/files", express.static(path.resolve(__dirname, "uploads")));
 app.use(routes);
 
 module.exports = app;
