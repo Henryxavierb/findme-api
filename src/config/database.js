99654b546f6f2bcab91ad4b9fe51b14911437389
Henrique Barros <henriquexavierb@gmail.com>
2021-01-04 15:27:31 -0300

    fix: add on ssl require property

--- src/config/database.js
+++ src/config/database.js
@@ -4,8 +4,8 @@ module.exports = {
   dialect: "postgres",
   database: process.env.DATABASE,
   host: process.env.DATABASE_HOST,
-  ssl: { rejectUnauthorized: false, require: true },
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   define: { timestamp: true, underscored: true },
+  ssl: { rejectUnauthorized: false, require: true },
 };
