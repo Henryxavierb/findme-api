0dfa21572f413f25ada21c780e924de7e1251afb
Henrique Barros <henriquexavierb@gmail.com>
2021-01-04 15:11:31 -0300

    fix: ssl postgres

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,7 @@ module.exports = {
   dialect: "postgres",
   database: process.env.DATABASE,
   host: process.env.DATABASE_HOST,
+  ssl: { rejectUnauthorized: false },
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   define: { timestamp: true, underscored: true },
