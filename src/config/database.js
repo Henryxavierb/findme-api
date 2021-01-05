f850cfec6fff06820b6a39701d816898d8dc19f7
Henrique Barros <henriquexavierb@gmail.com>
2021-01-04 23:39:29 -0300

    fix: rename environment variables

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 module.exports = {
   dialect: "postgres",
+  url: process.env.DATABASE_URL,
   database: process.env.DATABASE,
   host: process.env.DATABASE_HOST,
-  url: process.env.DEV_DATABASE_URL,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   define: { timestamp: true, underscored: true },
