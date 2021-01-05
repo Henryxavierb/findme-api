2f56e2c52c1fca3f0d0cc72469387f5b9120dfc6
Henrique Barros <henriquexavierb@gmail.com>
2021-01-05 00:36:28 -0300

    fix: database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const fs = require('fs');
+const path = require('path');
 
 module.exports = {
   dialect: "postgres",
   dialectOptions: {
     ssl: true,
-    ca: fs.readFileSync('../cert/csr.pem').toString(),
-    key: fs.readFileSync('../cert/key.pem').toString(),
-    cert: fs.readFileSync('../cert/cert.pem').toString(),
+    key: fs.readFileSync(__dirname + '..', 'cert', 'key.pem').toString(),
+    ca: fs.readFileSync(__dirname + '..', 'cert', 'csr.pem').toString(),
+    cert: fs.readFileSync(__dirname + '..', 'cert', 'cert.pem').toString(),
   },
   database: process.env.DATABASE,
   host: process.env.DATABASE_HOST,
