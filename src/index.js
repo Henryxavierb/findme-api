5d53b4668b756b27c33e9c7b3045bd594288fc20
Henrique Barros <henriquexavierb@gmail.com>
2021-01-05 11:49:56 -0300

    refactor: change upload file directory

--- src/index.js
+++ src/index.js
@@ -5,17 +5,8 @@ const path = require("path");
 require("./database");
 
 const app = express();
+
 app.use(cors());
-
-
-app.use(function (req, res, next) {
-  res.setHeader(
-    'Content-Security-Policy-Report-Only',
-    "default-src 'self'; font-src 'self'; img-src 'self' https://images.unsplash.com; script-src 'self'; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css; frame-src 'self'"
-  );
-  next();
-});
-
 app.use(express.json());
 
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
