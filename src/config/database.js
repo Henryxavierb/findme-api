4ffa7fdc285925230ec4fea5adbedc05151e4af8
Henrique Barros <henriquexavierb@gmail.com>
2021-01-04 23:36:11 -0300

    fix: add database url to postgres

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
-const fs = require('fs')
-const path = require('path')
 
 module.exports = {
   dialect: "postgres",
   database: process.env.DATABASE,
   host: process.env.DATABASE_HOST,
+  url: process.env.DEV_DATABASE_URL,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   define: { timestamp: true, underscored: true },
