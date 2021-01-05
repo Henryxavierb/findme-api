3223a0f53f5ddb7effbd087301dea11a0baa4a1c
Henrique Barros <henriquexavierb@gmail.com>
2021-01-05 00:45:55 -0300

    fix: database config

--- src/config/database.js
+++ src/config/database.js
@@ -5,11 +5,12 @@ const path = require('path');
 module.exports = {
   dialect: "postgres",
   dialectOptions: {
-    ssl: true,
+    ssl: { rejectUnauthorized: false },
     ca: fs.readFileSync('src/cert/csr.pem').toString(),
     key: fs.readFileSync('src/cert/key.pem').toString(),
     cert: fs.readFileSync('src/cert/cert.pem').toString(),
   },
+  url: process.env.DATABASE_URL,
   database: process.env.DATABASE,
   host: process.env.DATABASE_HOST,
   username: process.env.DATABASE_USERNAME,
